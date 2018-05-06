94824fc449a2a17045d8e2ab29fc729ae8480b21
Aman Arora <aman23091998@gmail.com>
2018-05-05 23:50:18 -0400

    Update gradle and dependencies

--- app/src/main/java/com/aroraaman/myshopify/myshopify/ui/OrderStatsFragment.java
+++ app/src/main/java/com/aroraaman/myshopify/myshopify/ui/OrderStatsFragment.java
@@ -1,6 +1,5 @@
 package com.aroraaman.myshopify.myshopify.ui;
 
-import android.arch.lifecycle.LifecycleFragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
@@ -8,6 +7,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 
 import javax.inject.Inject;
 
-public class OrderStatsFragment extends LifecycleFragment {
+public class OrderStatsFragment extends Fragment {
     @Inject
     ViewModelProvider.Factory mFactory;
 
